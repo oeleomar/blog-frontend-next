2a8093f9b06a980e0be890ebc1f5d2fe7043d85b
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-12-07 18:34:03 -0400

    ok

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,9 +1,5 @@
 import * as Styled from './styles';
 
 export function Home() {
-  return (
-    <Styled.Wrapper>
-      <h1>Ola</h1>
-    </Styled.Wrapper>
-  );
+  return <Styled.Wrapper></Styled.Wrapper>;
 }
