e082a564c33870832cc0812ff51eb38330c464a9
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-01-03 10:47:16 -0400

    Add Posts Template

---
+++ src/templates/PostsTemplate/index.tsx
@@ -0,0 +1,17 @@
+import { PostGrid } from 'components/PostGrid';
+import { PostStrapi } from 'shared-types/post-strapi';
+import { SettingsStrapi } from 'shared-types/settings-strapi';
+import { BaseTemplate } from 'templates/Base';
+
+export type PostsTemplateProps = {
+  settings: SettingsStrapi;
+  posts?: PostStrapi[];
+};
+
+export const PostsTemplate = ({ settings, posts = [] }: PostsTemplateProps) => {
+  return (
+    <BaseTemplate settings={settings}>
+      <PostGrid posts={posts} />
+    </BaseTemplate>
+  );
+};
