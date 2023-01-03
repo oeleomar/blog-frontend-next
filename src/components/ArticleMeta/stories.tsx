c25013e3f05322907250bfe2a2f53dc2351966f3
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-01-03 10:13:35 -0400

    Fixing article meta

--- src/components/ArticleMeta/stories.tsx
+++ src/components/ArticleMeta/stories.tsx
@@ -24,3 +24,27 @@ export const Template: Story<ArticleMetaProps> = (args) => {
     </div>
   );
 };
+
+export const NoAuthor: Story<ArticleMetaProps> = (args) => {
+  return (
+    <div>
+      <ArticleMeta {...args} author={undefined} />
+    </div>
+  );
+};
+
+export const NoCategories: Story<ArticleMetaProps> = (args) => {
+  return (
+    <div>
+      <ArticleMeta {...args} categories={[]} />
+    </div>
+  );
+};
+
+export const NoAuthorCategories: Story<ArticleMetaProps> = (args) => {
+  return (
+    <div>
+      <ArticleMeta {...args} author={undefined} categories={[]} />
+    </div>
+  );
+};
