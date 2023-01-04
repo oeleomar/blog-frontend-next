5f8fa5c1e160462c3d8b0d2adebf1adeb2f714b0
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-01-03 21:59:07 -0400

    Add next home and Post Page

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,5 @@ export default {
   url: 'https://strapi-v4-test.herokuapp.com/api/pages/?populate=deep&',
   siteName: 'Otávio Miranda',
   defaultSlug: 'landing-page',
+  graphQlUrl: 'http://localhost:1337/',
 };
