301a0ac3b7fe72cfce05c5a8245c906c2d3b66ba
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-01-05 20:14:56 -0400

    ok

--- src/templates/PostsTemplate/index.tsx
+++ src/templates/PostsTemplate/index.tsx
@@ -1,7 +1,9 @@
+import { useState, useEffect } from 'react';
 import { PostGrid } from 'components/PostGrid';
 import { PostStrapi } from 'shared-types/post-strapi';
 import { SettingsStrapi } from 'shared-types/settings-strapi';
 import { BaseTemplate } from 'templates/Base';
+import * as Styled from './styles';
 
 export type PostsTemplateProps = {
   setting: SettingsStrapi;
@@ -9,9 +11,28 @@ export type PostsTemplateProps = {
 };
 
 export const PostsTemplate = ({ setting, posts = [] }: PostsTemplateProps) => {
+  const [statePosts, setStatePosts] = useState([]);
+  const limit = 1;
+  let start = 0;
+
+  const handleLoadMorePosts = () => {
+    const post = posts.filter((post, i) => {
+      if (i >= start && i < limit) {
+        start = start + limit;
+        return post;
+      }
+    });
+    setStatePosts([...statePosts, post]);
+  };
+
   return (
     <BaseTemplate setting={setting}>
-      <PostGrid posts={posts} />
+      <PostGrid posts={statePosts} />
+      <Styled.ButtonContainer>
+        <Styled.Button onClick={handleLoadMorePosts}>
+          Carregar mais
+        </Styled.Button>
+      </Styled.ButtonContainer>
     </BaseTemplate>
   );
 };
