e88d4ea99805f524c85276fd3bc3f5383fe3b225
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-01-06 21:23:35 -0400

    Final toggle theme

--- src/components/ToggleTheme/index.tsx
+++ src/components/ToggleTheme/index.tsx
@@ -1,14 +1,39 @@
+import { BlogThemeContext } from 'contexts/BlogThemeContext';
+import { useState, useContext, useEffect } from 'react';
 import * as Styled from './styles';
 
-export type ToggleThemeProps = {
-  title?: string;
-};
+export const ToggleTheme = () => {
+  const { setTheme } = useContext(BlogThemeContext);
+  const [checked, setCheck] = useState(false);
 
-export const ToggleTheme = ({ title }: ToggleThemeProps) => {
+  useEffect(() => {
+    const localTheme = localStorage.getItem('theme');
+    if (!localTheme) return;
+    const newTheme = JSON.parse(localTheme);
+    if (newTheme.name === 'inverted') setCheck(true);
+  }, []);
+
+  useEffect(() => {
+    setTheme(checked ? 'inverted' : 'default');
+  }, [checked, setTheme]);
+
+  const handleChange = () => {
+    setCheck((s) => !s);
+    setTheme(checked ? 'inverted' : 'default');
+  };
+
   return (
     <Styled.Wrapper>
-      <h1>Oi</h1>
-      <p>{title}</p>
+      <Styled.Label>
+        Toggle light and dark mode
+        <Styled.Input
+          type="checkbox"
+          value="Dark mode active"
+          onChange={handleChange}
+          checked={checked}
+        />
+        <Styled.Slider></Styled.Slider>
+      </Styled.Label>
     </Styled.Wrapper>
   );
 };
