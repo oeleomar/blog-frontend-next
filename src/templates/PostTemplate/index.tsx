c7eba19480764b8425b37a94faf8adf81c703653
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-02-14 22:07:25 -0400

    ok

--- src/templates/PostTemplate/index.tsx
+++ src/templates/PostTemplate/index.tsx
@@ -4,6 +4,7 @@ import { PostTags } from 'components/PostTags';
 import { PostStrapi } from 'shared-types/post-strapi';
 import { SettingsStrapi } from 'shared-types/settings-strapi';
 import { BaseTemplate } from 'templates/Base';
+import { Comments } from 'components/Comments';
 
 export type PostTemplateProps = {
   setting: SettingsStrapi;
@@ -17,6 +18,13 @@ export const PostTemplate = ({ setting, post }: PostTemplateProps) => {
       <Styled.TagsContainer>
         <PostTags tags={post.tags} />
       </Styled.TagsContainer>
+
+      <Comments
+        title={post.title}
+        slug={post.slug}
+        id={post.id}
+        allowComments={post.allowComments}
+      />
     </BaseTemplate>
   );
 };
